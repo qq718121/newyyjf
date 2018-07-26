0e46dd5efab80c8e56b6e24d1d21d23f4f2b0e92
qq718121 <512854577@qq.com>
2018-07-26 18:35:03 +0800

    yyjf

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,4 @@
-var home = '#home';
+var home = '#homes';
 var server = '#server';
 var newscenter = '#newscenter';
 var load = '#download';
@@ -6,58 +6,62 @@ var cs_link = 'nav-link-style';
 var cs_active = 'nav-active-style';
 
 getList();
-$("[href='#home']").click();
+$("[href='#homes']").click();
+
+// function navHide() {
+//     $("#collapsibleNavbar").collapse('hide');
+// }
 
 function getList() {
-      var list = $('.u-nav-list').find('a');
-      list.each(function () {
-            $(this)
-                  .unbind()
-                  .bind('click', navClickHander);
-      })
+    var list = $('.u-nav-list').find('a');
+    list.each(function () {
+        $(this)
+            .unbind()
+            .bind('click', navClickHander);
+    });
 }
 
 function initStyle() {
-      var list = $('.u-nav-list').find('a');
-      list.each(function () {
-            $(this)
-                  .removeClass(cs_active)
-                  .addClass(cs_link);
-      })
+    var list = $('.u-nav-list').find('a');
+    list.each(function () {
+        $(this)
+            .removeClass(cs_active)
+            .addClass(cs_link);
+    });
 }
 function qr_toggle() {
-      var qr = $('.u-rq-code');
-      qr.fadeToggle("slow");
+    var qr = $('.u-rq-code');
+    qr.fadeToggle("slow");
 }
 function qr_is_toggle(key) {
-      var qr = $('.u-rq-code');
-      if (key == load) return;
-      if (!qr.is(":hidden")) {
-            qr.fadeOut("slow");
-      }
+    var qr = $('.u-rq-code');
+    if (key == load) return;
+    if (!qr.is(":hidden")) {
+        qr.fadeOut("slow");
+    }
 }
 
 function navClickHander() {
-      var key = $(this).attr('href');
-      initStyle.call(this, '');
-      qr_is_toggle(key);
-      $(this)
-            .removeClass(cs_link)
-            .addClass(cs_active);
-      switch (key) {
-            case home:
-                  console.log('home');
-                  break;
-            case server:
-                  console.log('server');
-                  break;
-            case newscenter:
-                  console.log('newscenter');
-                  break;
-            case load:
-                  qr_toggle();
-                  break;
-            default:
-                  break;
-      }
+    var key = $(this).attr('href');
+    initStyle.call(this, '');
+    qr_is_toggle(key);
+    $(this)
+        .removeClass(cs_link)
+        .addClass(cs_active);
+    switch (key) {
+        case home:
+            $("#collapsibleNavbar").collapse('hide');
+            break;
+        case server:
+            $("#collapsibleNavbar").collapse('hide');
+            break;
+        case newscenter:
+            $("#collapsibleNavbar").collapse('hide');
+            break;
+        case load:
+            qr_toggle();
+            break;
+        default:
+            break;
+    }
 }
